bf57061d9480f4821a0543b4bad9272953fde44f
Trent Piepho <tpiepho@gmail.com>
2021-04-21 03:45:01 -0700

    Auto-extract subs in adm_edit_srt
    If a .srt file doesn't exist, attempt to probe the source file for subs
    with ffprobe and extract them with ffmpeg.  Supports EIA-608 closed
    captions too, which are a little different than other subs.

--- addons/adm_edit_srt.py
+++ addons/adm_edit_srt.py
@@ -2,6 +2,8 @@
 # Copyright (C) 2021 Trent Piepho <tpiepho@gmail.com>
 
 import os.path
+import subprocess
+import codecs
 import sys
 import pysrt
 from fakeavidemux import Avidemux
@@ -29,13 +31,43 @@ shift caused by leading B-frames.
 ''')
     sys.exit(1)
 
+def getsrt(name):
+    srtname = os.path.splitext(name)[0] + '.srt'
+    if os.path.exists(srtname):
+        print(f"Reading for existing srt file '{srtname}'")
+        return pysrt.open(srtname)
+    if not os.path.exists(name):
+        print(f"Can't find {srtname} or {name}, giving up")
+        sys.exit(1)
+    probe = subprocess.run(
+        ['ffprobe', '-show_entries', 'stream=index', '-select_streams', 's', '-of', 'flat', name],
+        capture_output=True, check=True)
+    if probe.stdout.decode(sys.stdout.encoding).find('.index=') != -1:
+        print(f"Extracting subs from {name}")
+        cmd = ['ffmpeg', '-i', name, '-map', 's', '-f', 'srt', 'pipe:']
+        with subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL) as srtpipe:
+            subs = pysrt.SubRipFile()
+            subs.extend(subs.stream(codecs.getreader('UTF-8')(srtpipe.stdout)))
+            return subs
+    probe = subprocess.run( ['ffprobe', '-hide_banner', name], capture_output=True, check=True)
+    if probe.stderr.decode(sys.stdout.encoding).find('Closed Captions'):
+        print(f"Extracting EIA-608 Closed Captions from {name} (this make take awhile)")
+        cmd = ['ffmpeg', '-f', 'lavfi', '-i', f"movie={name}[out0+subcc]", '-map', 's', '-f', 'srt', 'pipe:']
+        with subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL) as srtpipe:
+            subs = pysrt.SubRipFile()
+            subs.extend(subs.stream(codecs.getreader('UTF-8')(srtpipe.stdout)))
+            return subs
+    else:
+        print(f"no subs in {name}")
+        return None
+
 # Read in Avidemux Project, which will create an adm object that has the file
 # and segment info
 proj = open(sys.argv[1]).read()
 exec(proj)
 
 # Read all files loaded in project with .srt extension as subtitles
-srt = [pysrt.open(os.path.splitext(f)[0] + '.srt') for f in adm.files]
+srt = [getsrt(f) for f in adm.files]
 
 subs = pysrt.SubRipFile() # Accumulated output subs
 time_us = 0               # Current output timestamp in microseconds
